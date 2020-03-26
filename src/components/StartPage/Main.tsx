76ecb7bb4781ba450a48ebef0813613c7e37b3ef
Koshlan <mykola.koshlan@gmail.com>
2020-03-26 14:21:15 +0200

    New things on page

--- src/components/StartPage/Main.tsx
+++ src/components/StartPage/Main.tsx
@@ -5,17 +5,18 @@ import Sidebar from './Sidebar/Sidebar';
 function Main() {
     return (
         <div id="user_page">
-            <div className="container-fluid">
+            <div className="container-fluid" >
                 <div className="row">
-                    <div className="col-md-3 ">
+                    <div className="col-md-4 col-sm-12">
                         <Sidebar/>
                     </div>
-                    <div className="col-md-9 col-sm-12">
-                        <Task/>
-                        <Task/>
-                        <Task/>
-                        <Task/>
-                        <Task/>
+                    <div className="col-md-8 col-sm-12">
+                        <Task priority={"red"}/>
+                        <Task priority={"red"}/>
+                        <Task priority={"yellow"}/>
+                        <Task priority={"yellow"}/>
+                        <Task priority={"green"}/>
+
                     </div>
                 </div>
             </div>
