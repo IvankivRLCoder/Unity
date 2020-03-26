9bdc63d34ebe9c08f37adbd5d2c8f09a32ebd4fa
Teodor <manemap.3@gmail.com>
2020-03-26 19:20:49 +0200

    Removed application.properties and other config files

--- src/main/java/com/example/dto/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dto;
-
-import com.example.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
