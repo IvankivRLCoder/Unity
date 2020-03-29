cd05e4892a5c108422309f0ada439604452be259
Teodor <manemap.3@gmail.com>
2020-03-29 15:01:22 +0300

    Revert "Merge branch 'feature/back-end-1' into functionality_for_registration"
    This reverts commit a64f7147368bc53dd5ba2a20543acf87d9772915.

--- src/main/java/com/example/error/EntityNotFountException.java
+++
@@ -1,9 +0,0 @@
-package com.example.error;
-
-public class EntityNotFountException extends RuntimeException {
-
-    public EntityNotFountException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/example/error/OverflowingTaskException.java
+++
@@ -1,9 +0,0 @@
-package com.example.error;
-
-public class OverflowingTaskException extends RuntimeException {
-
-    public OverflowingTaskException(String message) {
-        super(message);
-    }
-
-}
