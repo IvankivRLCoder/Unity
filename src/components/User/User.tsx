a885f30e3dd8da4156c788a894f78ba5bb618b61
kappa <dsadsa>
2020-03-31 13:07:14 +0300

    base user

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -2,21 +2,32 @@ import React from 'react';
 import Tree from './Tree/Tree';
 import UserInfo from './UserInfo/UserInfo';
 
-function User() {
-    return (
-        <div id="user_page">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-lg-8 col-12 order-2 order-lg-1">
-                        <Tree/>
-                    </div>
-                    <div className="col-lg-4 col-12 order-1 order-lg-2">
-                        <UserInfo/>
+class User extends React.Component {
+    userInfo: any;
+    user: any = {
+        name: 'Nazar',
+        surname: 'Koval',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut ' +
+            'labore et dolore magna aliqua. Ut enim ad minim veniam, quisnostrud exercitation ullamco laboris nisi ut aliquip ex ea',
+        achievements: []
+    };
+    constructor(options: any) {
+        super(options);
+        // this.userInfo = new UserInfo(this.user);
+    }
+    render() {return (<div id="user_page" style={{width: "100%"}}>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-8 col-12 order-2 order-lg-1">
+                            <Tree/>
+                        </div>
+                        <div className="col-lg-4 col-12 order-1 order-lg-2">
+                            <UserInfo/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    );
+            </div>);
+    }
 }
 
 export default User;
--- src/components/User/UserInfo/UserInfo.tsx
+++ src/components/User/UserInfo/UserInfo.tsx
@@ -2,17 +2,70 @@ import React from 'react';
 import avatar from './avatar.png';
 import './UserInfo.scss';
 
-function UserInfo() {
-    return (
-        <div className="user-info-block">
-            <img src={avatar} className="user-info-img" alt="Avatar"/>
-            <h3 className="user-name">Nazar Koval</h3>
-            <p className="user-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quisnostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            </p>
-        </div>
-    );
+class UserInfo extends React.Component {
+    state = {
+        name: "",
+        surname: "",
+        description: "",
+        avatar: "",
+        editMode: false
+    };
+
+    constructor(options:any) {
+        super(options);
+        console.log(options);
+        // this.setState({
+        //     name: options.name,
+        //     surname: options.surname,
+        //     decription: options.description
+        // })
+        this.state.name = options.name;
+        this.state.surname = options.surname;
+        this.state.description = options.description;
+    }
+
+    handleMode = (event: React.FormEvent<HTMLFormElement>):void => {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <div className={"user-info-block " + (this.state.editMode ? 'hide' : 'show')}>
+                    <img src={avatar} className="user-info-img" alt="Avatar"/>
+                    <h3 className="user-name">{this.state.name} {this.state.surname}</h3>
+                    <p className="user-description">
+                        {this.state.description}
+                    </p>
+                    <div className={'action-panel'}>
+                        <button type={"button"} className={"btn btn-danger"} onClick={e => this.setState({editMode: true})}>Edit</button>
+                    </div>
+                </div>
+                <div className={"user-info-block " + (this.state.editMode ? 'show' : 'hide')}>
+                    <img src={avatar} className="user-info-img" alt="Avatar"/>
+                    <div className={"row"} style={{paddingTop: "15px"}}>
+                        <div className={"col-md-6"}>
+                            <input className={"form-control "} placeholder={"Name"} defaultValue={this.state.name}/>
+                        </div>
+                        <div className={"col-md-6"}>
+                            <input className={"form-control"} placeholder={"Surname"} defaultValue={this.state.surname}/>
+                        </div>
+                    </div>
+                    <textarea className={"form-control user-description"} placeholder={"Enter your description"} defaultValue={this.state.description}>
+                    </textarea>
+                    <div className={'action-panel'} style={{paddingTop: "15px"}}>
+                        <button className={"btn btn-primary"} style={{marginRight: "15px"}}
+                                onClick={e => this.setState({editMode: false})}>Cancel
+                        </button>
+                        <button className={"btn btn-success"}
+                                onClick={e => this.setState({editMode: false})}>Save
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default UserInfo;
