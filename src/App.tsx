0491885385b6fe8cd4e1f54a8b963fe680066a40
Oleksii Havryshkiv <oleksii.havryshkiv@agiliway.com>
2020-04-12 21:15:58 +0300

    Login\registration\logout integration with back-end. Change icons on header for different users roles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import User from './components/User/User';
 import Login from './components/Login_Registration/Login';
 import Registration from './components/Login_Registration/Registration';
 import Main from './components/StartPage/Main';
 import Task from './components/Task/Task';
-
 import Layout from './hoc/Layout/Layout';
 
 import './App.scss';
+import Auth from "./utils/Auth/Auth";
 
 function App() {
   return (
     <Layout>
         <Route path="/user" exact component={User} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/registration" exact component={Registration} />    
         <Route path="/" exact component={Main} />
         <Route path="/task" exact component={Task}/>
+        <Route path="/login" render={() => (!Auth.isLoggedIn ? <Login/> : <Redirect to="/" />)} />
+        <Route path="/registration" render={() => (!Auth.isLoggedIn ? <Registration/> : <Redirect to="/" />)} />
     </Layout>
-    );
+  );
 }
 
 export default App;
